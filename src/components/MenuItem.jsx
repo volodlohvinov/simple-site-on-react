d99d554e55569ed7432535fa0b4d80258c59c723
Volodymyr Lohvinov <vladimirpravda2@gmail.com>
2023-09-04 22:24:19 +0200

    added changes

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import './MenuItem.scss'
+import { Link } from "react-router-dom"
 
 export function MenuItem() {
+    const linkStyle = {
+        textDecoration: 'none', 
+        color: 'inherit' 
+    };
     return <ul className="MenuItem-container">
-        <li className="MenuItem">Main</li>
-        <li className="MenuItem">Q&A</li>
-        <li className="MenuItem">Materials</li>
-        <li className="MenuItem">Contacts</li>
+        <li className="MenuItem"><Link style={linkStyle} to='/'>Main</Link></li>
+        <li className="MenuItem"><Link style={linkStyle} to='/qa'>Q&A</Link></li>
+        <li className="MenuItem"><Link style={linkStyle} to='/materials'>Materials</Link></li>
+        <li className="MenuItem"><Link style={linkStyle} to='/contacts'>Contacts</Link></li>
     </ul>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import ReactDOM  from 'react-dom/client'
 import App from './App.jsx'
+import './index.scss'
+import { BrowserRouter, RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom'
+import { MenuItem } from './components/MenuItem.jsx'
 
 const rootEl = document.getElementById("root")
 const root = ReactDOM.createRoot(rootEl)
 
-root.render(<App/> )
+const router = createHashRouter([{
+    path: '/',
+    element: <App/>
+},
+{
+    path: '/qa',
+    element: <div>
+    <MenuItem /> 
+    <p className='question'>When did Steam appers?</p>
+    <p className='answer'>It was launched as a software client in September 2003 to provide game updates automatically 
+    for Valve's games, and expanded to distributing third-party titles in late 2005.</p>
+    <p className='question'>How many games in Steam?</p>
+    <p className='answer'>Steam's US library contains 92,196 titles across all categories. This includes add-on content like DLC, 
+    mods and soundtracks. 50,361 games are available on Steam as of March 2021.</p>
+    </div>
+},
+{
+    
+    path: '/materials',
+    element: <div>
+    <MenuItem /> 
+    <p className='name'>Most our popular games:</p>
+   <a className='links' href="https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/">CS:GO</a>
+   <a className='links' href="https://store.steampowered.com/app/570/Dota_2/">Dota 2</a>
+   <a className='links' href="https://store.steampowered.com/app/1172470/Apex_Legends/">Apex Legends</a>
+   <a className='links' href="https://store.steampowered.com/app/578080/PUBG_BATTLEGROUNDS/">PUBG</a>
+    </div>
+},
+   { path: '/contacts',
+    element: <div>
+    <MenuItem /> 
+    <p className='name'>If u wanna contact us use this link:</p>
+    <a className='links' href="https://help.steampowered.com/en/">Steam Support</a>
+    </div>
+}
+])
+
+root.render(
+    <RouterProvider router={router} />
+)
